66ea53e0c4ce0bb4dca27a7245aeb83de99758c2
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-26 19:29:15 +0200

    set up base routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import SafeChat from "./containers/SafeChat/SafeChat";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<SafeChat />
-		</div>
+		<Router>
+			<div className="App">
+				<SafeChat />
+			</div>
+		</Router>
 	);
 }
 
--- src/containers/SafeChat/SafeChat.js
+++ src/containers/SafeChat/SafeChat.js
@@ -1,10 +1,21 @@
 import Login from "../Login/Login";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./SafeChat.css";
 
 export default function SafeChat() {
 	return (
 		<div className="SafeChat">
-			<Login />
+			<Switch>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/main">
+					<div>Main Chat</div>
+				</Route>
+				<Route path="/">
+					<Redirect to="/login" />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
