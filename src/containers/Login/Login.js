729ee953a59603daadd771129ce677a89a83f5a1
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-26 19:46:04 +0200

    prevent form submission and add more essential dependencies

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import { uuid } from "uuidv4";
 import ContentBox from "../../components/ContentBox/ContentBox";
 import Input from "../../components/Input/Input";
 import "./Login.css";
 
 export default function Login() {
 	const [usernameState, setUsernameState] = useState("");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 	return (
 		<ContentBox height="100%" width="100%">
 			<div className="Login">
-				<form className="Login-form">
+				<form className="Login-form" onSubmit={handleSubmit}>
 					<label for="usernameinput" className="Login-label">
 						Enter Your Username
 					</label>
