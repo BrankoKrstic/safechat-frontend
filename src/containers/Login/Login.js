f1db6f8366f6b7446ee2cfc1271c7a4b04c7ab71
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-27 21:24:38 +0200

    Add base logic for creating username and password

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { uuid } from "uuidv4";
+import { v4 as uuid } from "uuid";
 import ContentBox from "../../components/ContentBox/ContentBox";
 import FormInput from "../../components/FormInput/FormInput";
 import FormButton from "../../components/FormButton/FormButton";
@@ -9,13 +9,18 @@ export default function Login() {
 	const [usernameState, setUsernameState] = useState("");
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		if (usernameState.length === 0) return;
+		const username = usernameState;
+		setUsernameState("");
+		const userId = uuid();
+		console.log({ username, userId });
 	};
 	return (
 		<div className="Login">
 			<ContentBox>
 				<div className="LoginContent">
 					<form className="Login-form" onSubmit={handleSubmit}>
-						<label for="usernameinput" className="Login-label">
+						<label htmlFor="usernameinput" className="Login-label">
 							Enter Your Username
 						</label>
 						<FormInput
