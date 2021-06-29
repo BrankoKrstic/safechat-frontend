151f66e24751f724392b0f5019efd16a8b7dff38
BrankoKrstic <brankokrstic94@gmail.com>
2021-06-29 11:29:26 +0200

    style participant widget

--- src/containers/ChatMain/ChatSidebar/UserWidget/UserWidget.js
+++ src/containers/ChatMain/ChatSidebar/UserWidget/UserWidget.js
@@ -3,6 +3,7 @@ import "./UserWidget.css";
 export default function UserWidget(props) {
 	return (
 		<div className={props.isEven ? "UserWidget" : "UserWidget Even"}>
+			<div className="UserWidget-online"></div>
 			{props.username}
 		</div>
 	);
