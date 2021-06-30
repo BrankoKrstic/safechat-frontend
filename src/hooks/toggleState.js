efafa640eceae3b28cb35e88eb5e07ae4830870e
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-01 01:42:18 +0200

    implement custom toggle function

---
+++ src/hooks/toggleState.js
@@ -0,0 +1,11 @@
+import useState from "react";
+
+const useToggleState = (boolean) => {
+	const [state, setState] = useState(boolean);
+	const toggle = () => {
+		setState(!state);
+	};
+	return [state, toggle];
+};
+
+export default useToggleState;
