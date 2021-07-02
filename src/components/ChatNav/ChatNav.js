54484df69e0aaa597a265f1fc11cff88f6dc1f76
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-03 01:19:28 +0200

    Add responsive navbar

--- src/components/ChatNav/ChatNav.js
+++ src/components/ChatNav/ChatNav.js
@@ -1,5 +1,13 @@
 import "./ChatNav.css";
 
-export default function ChatNav() {
-	return <div className="ChatNav"></div>;
+export default function ChatNav(props) {
+	return (
+		<div className="ChatNav">
+			<div className="ChatNav-logo">SafeChat</div>
+			<button
+				onClick={props.navControl}
+				className="ChatNav-button"
+			></button>
+		</div>
+	);
 }
